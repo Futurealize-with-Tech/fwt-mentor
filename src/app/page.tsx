aeab4395ac63ea0f3ee905e379141e8b93034861
HASURO <rentaro170928@icloud.com>
2024-03-28 08:46:37 +0900

    add: root page #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-export default function Home() {
+import SelectMentorForm from '@/components/ui/form/SelectMentorForm';
+import styles from './page.module.scss';
+
+export default async function Home() {
   return (
-    <></>
+    <div className={styles['main']}>
+      <SelectMentorForm />
+    </div>
   )
 }
