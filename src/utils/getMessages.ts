6dd14ab4690388fa072fce017ca7c0fa040f5e77
HASURO <rentaro170928@icloud.com>
2024-03-29 16:12:59 +0900

    hotfix: bug #5

--- src/utils/getMessages.ts
+++ src/utils/getMessages.ts
@@ -1,5 +1,7 @@
+import { mentorKey } from "@/lib/key/key";
+
 export const getMessage = async () => {
-  const mentorId =  localStorage.getItem('mentorId');
+  const mentorId = localStorage.getItem(mentorKey);
   try {
     const res = await fetch(`https://api.futurealize-with.tech/api/v1/mentor/${mentorId}`);
     return res.json();
