aa247e20e530abfee7ef495578969f10fb3c8990
HASURO <rentaro170928@icloud.com>
2024-04-07 14:25:11 +0900

    add: card index

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,12 +1,20 @@
 'use client';
 
+import { Inter } from 'next/font/google'
+import { useEffect } from "react";
+import { useAtom } from "jotai";
 import { mentorKey } from "@/lib/key/key";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { isScrollInvalidAtom } from '@/atoms/atoms';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootTemplate({ children } : { children: React.ReactNode }) {
   const pathname = usePathname();
   const router = useRouter();
+  const [isScrollInvalid] = useAtom(isScrollInvalidAtom);
 
   useEffect(() => {
     const currentMentorId = localStorage.getItem(mentorKey);
@@ -17,5 +25,25 @@ export default function RootTemplate({ children } : { children: React.ReactNode 
     }
   }, [pathname]);
 
-  return children;
+  return (
+    <body
+      className={inter.className}
+      style={{ overflow: isScrollInvalid ? 'hidden' : '' }}
+    >
+      {children}
+      <ToastContainer
+        position='bottom-center'
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        className='toast'
+      />
+    </body>
+  );
 }
