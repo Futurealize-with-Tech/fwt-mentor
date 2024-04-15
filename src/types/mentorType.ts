69c0a27b341d3af97a13586ef9ab7d0a4b8871f0
HASURO <rentaro170928@icloud.com>
2024-04-15 22:49:56 +0900

    add: type

--- src/types/mentorType.ts
+++ src/types/mentorType.ts
@@ -2,10 +2,15 @@ import { MentorCourseType } from "./courseType";
 import { MentorRegionType } from "./regionType";
 
 export type MentorType = {
-    id: number,
-    name: string,
-    imageUrl?: string,
-    courses: MentorCourseType[],
-    regions: MentorRegionType[],
+  id: number,
+  name: string,
+  imageUrl?: string,
+  courses: MentorCourseType[],
+  regions: MentorRegionType[],
 };
 
+export type MessageMentorType = {
+  id: number,
+  name: string,
+  imageUrl?: string,
+};
