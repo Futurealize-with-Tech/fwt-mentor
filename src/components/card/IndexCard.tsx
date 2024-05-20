77d7ddda8b3394c3addeea674d73eb178927c1d7
Nozomi Fukawa <noppi5959@gmail.com>
2024-05-20 23:21:54 +0900

    aaaaa

--- src/components/card/IndexCard.tsx
+++ src/components/card/IndexCard.tsx
@@ -1,42 +1,80 @@
-'use client';
+"use client";
 
 import React, { useContext } from "react";
 import Image from "next/image";
 import { MessageType } from "@/types/messageType";
-import cardDesign from '@/public/card/card-design.jpg';
-import cardTitle from '@/public/text/card-title.png';
-import messageText from '@/public/text/message-text.png';
-import memberText from '@/public/text/member-text.png';
-import styles from './card.module.scss';
+import cardDesign from "@/public/card/card-design.jpg";
+import cardTitle from "@/public/text/card-title.png";
+import messageText from "@/public/text/message-text.png";
+import memberText from "@/public/text/member-text.png";
+import styles from "./card.module.scss";
 import { CurrentMentorContext } from "@/middleware/CurrentMentorProvider";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
-export default function IndexCard({ message } : { message: MessageType }) {
+export default function IndexCard({ message }: { message: MessageType }) {
   const currentMentor = useContext(CurrentMentorContext);
 
+  const downLoadImg = () => {
+    //画像化したいdomをidで取得
+    const target = document.getElementById("target-container");
+    if (target == null) return;
+
+    html2canvas(target, { scale: 1 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/svg", 1.0);
+      let pdf = new jsPDF();
+      pdf.addImage(imgData, "SVG", 0, 0, canvas.width / 4, canvas.height / 4);
+      pdf.save(`test.pdf`);
+    });
+  };
+
   return (
-    <div className={styles['card-container']}>
-      <Image src={cardDesign} alt='card' width={800} className={styles['card-bg-img']} />
-      <div className={styles['card-front-container']}>
-        <Image src={cardTitle} alt='title' width={600} className={styles['card-title-img']} />
-        <div className={styles['card-mentor-area']}>
-          {currentMentor?.name}
-          <div className={styles['card-mentor-area-decorate']}>殿</div>
-        </div>
-        <div className={styles['card-message-area']}>
-          <Image src={messageText} alt='message' width={80} className={styles['card-message-area-title-img']} />
-          <p className={styles['card-message-area-body']}>
-            {message.body.split('\n').map((text, index) => (
-              <p key={index}>
-                {text}
-              </p>
-            ))}
-          </p>
-        </div>
-        <div className={styles['card-member-area']}>
-          <Image src={memberText} alt='message' width={80} className={styles['card-member-area-title-img']} />
-          {message.memberName}
+    <>
+      <div className={styles["card-container"]} id="target-container">
+        <Image
+          src={cardDesign}
+          alt="card"
+          width={800}
+          className={styles["card-bg-img"]}
+        />
+        <div className={styles["card-front-container"]}>
+          <Image
+            src={cardTitle}
+            alt="title"
+            width={600}
+            className={styles["card-title-img"]}
+          />
+          <div className={styles["card-mentor-area"]}>
+            {currentMentor?.name}
+            <div className={styles["card-mentor-area-decorate"]}>殿</div>
+          </div>
+          <div className={styles["card-message-area"]}>
+            <Image
+              src={messageText}
+              alt="message"
+              width={80}
+              className={styles["card-message-area-title-img"]}
+            />
+            <p className={styles["card-message-area-body"]}>
+              {message.body.split("\n").map((text, index) => (
+                <p key={index}>{text}</p>
+              ))}
+            </p>
+          </div>
+          <div className={styles["card-member-area"]}>
+            <Image
+              src={memberText}
+              alt="message"
+              width={80}
+              className={styles["card-member-area-title-img"]}
+            />
+            {message.memberName}
+          </div>
         </div>
       </div>
-    </div>
+      <button type="button" onClick={downLoadImg}>
+        ダウンロード
+      </button>
+    </>
   );
 }
--- src/components/message/MessageIndexBox.tsx
+++ src/components/message/MessageIndexBox.tsx
@@ -1,35 +1,35 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { MessageType } from '@/types/messageType';
-import { messageIcons } from '@/lib/data/icons';
-import { iconColors } from '@/lib/data/colors';
-import styles from './message.module.scss';
-import SaveCardButton from '../ui/button/SaveCardButton';
-import OpenCardTextButton from '../ui/button/OpenCardTextButton';
+import React from "react";
+import { MessageType } from "@/types/messageType";
+import { messageIcons } from "@/lib/data/icons";
+import { iconColors } from "@/lib/data/colors";
+import styles from "./message.module.scss";
+import SaveCardButton from "../ui/button/SaveCardButton";
+import OpenCardTextButton from "../ui/button/OpenCardTextButton";
 
-export default function MessageIndexBox({ message } : { message: MessageType }) {
+export default function MessageIndexBox({ message }: { message: MessageType }) {
   const iconNum = Math.floor(Math.random() * messageIcons.length);
   const iconColorNum = Math.floor(Math.random() * iconColors.length);
   const Icon = messageIcons[iconNum];
   const color = iconColors[iconColorNum];
 
   return (
-    <div className={styles['box-container']}>
-      <div className={styles['box-wrapper']}>
-        <div className={styles['box-top-container']}>
+    <div className={styles["box-container"]}>
+      <div className={styles["box-wrapper"]}>
+        <div className={styles["box-top-container"]}>
           <div
-            className={styles['box-icon-box']}
+            className={styles["box-icon-box"]}
             style={{ backgroundColor: `rgba(var(${color}), 0.4)` }}
           >
             <Icon />
           </div>
           <SaveCardButton message={message} />
         </div>
-        <p className={styles['box-name']}>{message.memberName}</p>
-        <p className={styles['box-body-container']}>
-          {message.body.split('\n').map((item, key) => (
-            <p className={styles['box-body']} key={key}>
+        <p className={styles["box-name"]}>{message.memberName}</p>
+        <p className={styles["box-body-container"]}>
+          {message.body.split("\n").map((item, key) => (
+            <p className={styles["box-body"]} key={key}>
               {item}
             </p>
           ))}
@@ -39,3 +39,5 @@ export default function MessageIndexBox({ message } : { message: MessageType }) 
     </div>
   );
 }
+
+("");
